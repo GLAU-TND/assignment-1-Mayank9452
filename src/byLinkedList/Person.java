096f938ddb89efd1dd841f365dae771ff9f8f731
Mayank Kumar Singh <mayank.singh_cs18@gla.ac.in>
2020-02-19 19:42:45 +0530

    field created: firstName,lastName,contactNumber,mailId

--- src/byLinkedList/Person.java
+++ src/byLinkedList/Person.java
@@ -1,4 +1,11 @@
 package byLinkedList;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Long> contactNumber;
+    private String mailId;
+
 }
