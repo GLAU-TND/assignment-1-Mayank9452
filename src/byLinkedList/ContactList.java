55333ad24ad41ad3d15da3c9765f0f57d4c10d64
Mayank Kumar Singh <mayank.singh_cs18@gla.ac.in>
2020-02-23 00:35:10 +0530

    class created: ContactList that implements ADT interface of class Person type

---
+++ src/byLinkedList/ContactList.java
@@ -0,0 +1,24 @@
+package byLinkedList;
+
+public class ContactList implements ADT<Person> {
+
+    @Override
+    public void add(Person data) {
+
+    }
+
+    @Override
+    public void remove(Person data) {
+
+    }
+
+    @Override
+    public Person search(String firstName) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
